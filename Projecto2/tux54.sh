7bb8ba3ee3a44bfe2a77f42d9d2e396baf41e155
Pedro <pedro21fcp@gmail.com>
2017-12-17 17:54:24 +0000

    update tux 54 config

--- Projecto2/tux54.sh
+++ Projecto2/tux54.sh
@@ -7,3 +7,7 @@ route add default gw 172.16.51.254
 echo 1 >/proc/sys/net/ipv4/ip_forward
 echo 0 >/proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
 
+iptables –t nat –A POSTROUTING -o eth1 –j MASQUERADE
+iptables –A FORWARD –i eth1 –m state --state NEW,INVALID –j DROP
+iptables –L
+iptables –t nat –L 
